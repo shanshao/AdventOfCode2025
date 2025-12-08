c2d90861d8771178a3ce711f4246750bceb473e1
Shan Shao <>
2025-12-08 16:19:18 -0500

    Finish day 2 part 2

--- Challenges/Day 2/day2.py
+++ Challenges/Day 2/day2.py
@@ -5,13 +5,23 @@ class Day2Challenge:
         base = os.path.dirname(os.path.abspath(__file__))
         self.file_path = os.path.join(base, "input.txt")
 
-    def day_2(self):
+    def day_2_part_1(self):
         invalid_count = 0
         ranges = self.get_input()
         for ids in ranges:
             lower, upper = ids.split("-")
             for i in range(int(lower), int(upper) + 1):
                 if self.has_repeating_digits(i):
+                    invalid_count += i
+        return invalid_count
+    
+    def day_2_part_2(self):
+        invalid_count = 0
+        ranges = self.get_input()
+        for ids in ranges:
+            lower, upper = ids.split("-")
+            for i in range(int(lower), int(upper) + 1):
+                if self.has_multiple_repeating_digits(i):
                     invalid_count += i
         return invalid_count
 
@@ -22,6 +32,17 @@ class Day2Challenge:
         half = len(s) // 2
         return s[:half] == s[half:]
     
+    def has_multiple_repeating_digits(self, n: int) -> bool:
+        s = str(abs(n))
+        if self.has_repeating_digits(n): return True # "1212" case  
+        if len(set(s)) == 1 and len(s) > 1: return True # "11111" case  
+        for i in range(1, len(s) - 1):
+            block = s[:i]
+            repeats = len(s) // len(block)
+            if block * repeats == s:
+                return True
+        return False
+    
     def get_input(self):
         file_object = open(self.file_path, "r")
         input_line = file_object.readlines()
@@ -29,4 +50,4 @@ class Day2Challenge:
         ranges = input_line[0].strip().split(",")
         return ranges
     
-challenge = print(Day2Challenge().day_2())
+challenge = print(Day2Challenge().day_2_part_2())
