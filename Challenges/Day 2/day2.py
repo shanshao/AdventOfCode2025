53539580f035b409e61e6fbd5810a0b095272a00
Shan Shao <>
2025-12-08 02:37:52 -0500

    Finish Day 2 part 2

--- Challenges/Day 2/day2.py
+++ Challenges/Day 2/day2.py
@@ -6,13 +6,27 @@ class Day2Challenge:
         self.file_path = os.path.join(base, "input.txt")
 
     def day_2(self):
-        print(self.get_input())
+        invalid_count = 0
+        ranges = self.get_input()
+        for ids in ranges:
+            lower, upper = ids.split("-")
+            for i in range(int(lower), int(upper) + 1):
+                if self.has_repeating_digits(i):
+                    invalid_count += i
+        return invalid_count
 
-
+    def has_repeating_digits(self, n: int) -> bool:
+        s = str(abs(n))
+        if len(s) % 2 != 0:
+            return False
+        half = len(s) // 2
+        return s[:half] == s[half:]
+    
     def get_input(self):
         file_object = open(self.file_path, "r")
         input_line = file_object.readlines()
         file_object.close()
-        return input_line
+        ranges = input_line[0].strip().split(",")
+        return ranges
     
 challenge = print(Day2Challenge().day_2())
