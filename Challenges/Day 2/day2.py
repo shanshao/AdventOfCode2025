12cb45dcf3cf94a13b7b2e8b7525d4f21cff0f9a
Shan Shao <>
2025-12-08 02:06:59 -0500

    Add skeleton

--- Challenges/Day 2/day2.py
+++ Challenges/Day 2/day2.py
@@ -1,2 +1,18 @@
+import os
+
 class Day2Challenge:
-    pass
+    def __init__(self):
+        base = os.path.dirname(os.path.abspath(__file__))
+        self.file_path = os.path.join(base, "input.txt")
+
+    def day_2(self):
+        print(self.get_input())
+
+
+    def get_input(self):
+        file_object = open(self.file_path, "r")
+        input_line = file_object.readlines()
+        file_object.close()
+        return input_line
+    
+challenge = print(Day2Challenge().day_2())
