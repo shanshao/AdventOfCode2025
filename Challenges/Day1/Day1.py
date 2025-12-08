b0e28a3381927012c280306c7f765568a3842b0d
Shan Shao <>
2025-12-08 01:32:04 -0500

    Day 1 Part 1

--- Challenges/Day1/Day1.py
+++ Challenges/Day1/Day1.py
@@ -1,5 +1,38 @@
-def day_1():
-    print("Welcome to Day 1 Challenge!")
-    pass
+import os
+class Day1Challenge:
 
-day_1()
+    def __init__(self):
+        self.dial = 50
+        base = os.path.dirname(os.path.abspath(__file__))
+        self.file_path = os.path.join(base, "input.txt")
+
+    def day_1(self):
+        password = 0
+        rotations = self.get_input()
+        for rotation in rotations:
+            direction, distance = rotation
+            if direction == "L":
+                self.dial -= distance
+            elif direction == "R":
+                self.dial += distance
+
+            self.dial = self.dial % 100     
+            if self.dial == 0:
+                password += 1
+        return password
+
+    def get_input(self):
+        rotations = []
+        file_object = open(self.file_path, "r")
+        input_lines = file_object.readlines()
+        file_object.close()
+
+        for line in input_lines:
+            direction = line[0]
+            distance = int(line[1:])   
+            rotations.append((direction, distance))
+
+        return rotations
+
+
+challenge = print(Day1Challenge().day_1())
