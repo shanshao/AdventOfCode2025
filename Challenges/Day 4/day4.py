d1ed865117edf50fab094962814bdff59fe55491
Shan Shao <sshao25@gmail.com>
2025-12-09 01:30:22 -0500

    Day 4 skeleton

---
+++ Challenges/Day 4/day4.py
@@ -0,0 +1,11 @@
+class Day4Challenge:
+    def __init__(self, file_path="Challenges/Day 4/input.txt"):
+        self.file_path = file_path
+
+    def part1(self):
+        # Implement the logic for part 1 of Day 4 challenge
+        pass
+
+    def part2(self):
+        # Implement the logic for part 2 of Day 4 challenge
+        pass
