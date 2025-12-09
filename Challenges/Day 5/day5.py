f680ce9e74da81a720588028f221261761095a6d
Shan Shao <sshao25@gmail.com>
2025-12-09 02:21:59 -0500

    Day 5 skeleton

---
+++ Challenges/Day 5/day5.py
@@ -0,0 +1,11 @@
+class Day5Challenge:
+    def __init__(self):
+        self.file_path = "Challenges/Day 5/input.txt"
+
+    def part1(self):
+        # Implement the logic for part 1 of Day 5 challenge
+        pass
+
+    def part2(self):
+        # Implement the logic for part 2 of Day 5 challenge
+        pass
