8f7832b9bd493d89a2c2324989b7389b093ba695
Shan Shao <sshao25@gmail.com>
2025-12-09 21:38:34 -0500

    Day 7 skeleton

---
+++ Challenges/Day 7/day7.py
@@ -0,0 +1,9 @@
+class Day7Challenge:
+    def init__(self, file_path):
+        self.file_path = "Challenges/Day 6/input.txt"
+
+    def part1(self):
+        pass
+    
+    def part2(self):
+        pass    
