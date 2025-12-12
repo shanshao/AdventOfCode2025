12c35bcb007ac1eb1d75b3dc862cbfc8e4e7f34a
Shan Shao <sshao25@gmail.com>
2025-12-12 03:02:16 -0500

    Day 8 skeleton

---
+++ Challenges/Day 8/day8.py
@@ -0,0 +1,15 @@
+class Day8Challenge:
+    def __init__(self):
+        self.input_data = self.load_input()
+
+    def load_input(self):
+        with open('Challenges/Day 8/input.txt', 'r') as file:
+            return file.read().strip().splitlines()
+
+    def part1(self):
+        pass
+
+    def part2(self):
+        pass
+    
+challenge = Day8Challenge().part1()
